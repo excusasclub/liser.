8246b4d9d777852a962b71a499f2e425b785a804
excusasclub <elclubdelaexcusa@gmail.com>
2025-09-30 07:47:04 +0200

    cambios header

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./liser2/**/*.{html,js}",
+    "./accounts/**/*.{html,js}",
+    "./lists/**/*.{html,js}",
+    "./catalog/**/*.{html,js}",
+    "./common/**/*.{html,js}",
+    "./analytics/**/*.{html,js}",
+    "./api/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
